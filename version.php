1b837b1786b5a5ca8830131f274f571cb3f4c66c
Jean-Michel Vedrine <vedrine@vedrine.org>
2018-04-10 17:35:33 +0200

    Forgot to correctly bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_formulas';
-$plugin->version   = 2018030100;
+$plugin->version   = 2018030101;
 
 $plugin->cron      = 0;
 $plugin->requires  = 2013101800;
